3a6aee27170a0b1f746e9c16bae0b58abb45c318
njorogejeff <njorogejeff254@gmail.com>
2021-04-14 16:04:50 +0300

    Admin Dashboard Template
    Imported static files of the admin template.

--- ems/urls.py
+++ ems/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from ems import settings
+from spve import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('demo', views.demopage),
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- spve/views.py
+++ spve/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def demopage(request):
+    return render(request, 'spve/demopage.html')
